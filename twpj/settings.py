3006bd91731d63b7d617a2dc0faefcbfa3e1de0a
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-12-10 19:22:45 +0900

    userModel ok

--- twpj/settings.py
+++ twpj/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'core',
+    'userauths',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+AUTH_USER_MODEL='userauths.User'
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
